aec4b1960813108e94fb4528a0ef06faa3ea0ac5
Felipe Echeverri <feloeche@gmail.com>
2021-09-29 14:58:20 -0500

    curso django

---
+++ platzigram/posts/apps.py
@@ -0,0 +1,9 @@
+#Post apliccation module
+
+from django.apps import AppConfig
+
+
+class PostsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'posts'
+    varbose_name = 'Posts'
